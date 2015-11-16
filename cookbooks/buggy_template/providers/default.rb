44d73121954422a09459cc85a394275b6d5eec56
Chase Bolt <cbolt@denirostaff.com>
2015-11-16 10:40:17 -0800

    just output the hash

--- cookbooks/buggy_template/providers/default.rb
+++ cookbooks/buggy_template/providers/default.rb
@@ -5,8 +5,7 @@ def load_current_resource
 end
 
 action :create do
-  puts "\n\nnumber: #{new_resource.number}"
-  puts "\n\nhash: #{new_resource.to_hash}"
+  puts "\n\n#{new_resource.to_hash}"
   template new_resource.name do
     source 'nail.erb'
     variables new_resource.to_hash
